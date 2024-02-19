1d4635e1a760ae9ba5e5176cddead6eca684bb9a
adammushy <adamprosper99@gmail.com>
2024-02-19 17:20:21 +0300

    updates in text  field model message

--- social_management/models.py
+++ social_management/models.py
@@ -40,7 +40,7 @@ class Message(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
                               null=True, related_name='message_sender')
-    text = models.CharField(max_length=200, blank=True)
+    text = models.TextField(blank=True)
     # attachment = models.FileField(blank=True)
     conversation_id = models.ForeignKey(Conversation, on_delete=models.CASCADE,)
     timestamp = models.DateTimeField(auto_now_add=True)
